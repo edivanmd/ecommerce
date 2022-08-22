41e4bc88a340a13a22c004106448d54c59e51c85
edivanmd <edivanmd@gmail.com>
2022-08-22 16:22:07 +1000

    Cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 
 import './App.css';
 
@@ -34,7 +34,8 @@ function App() {
   return (
     <div className="App">
       <Navbar totalItems={cart.total_items} />
-      <Products products={products} onAddToCart={handleAddToCart} />
+      {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+      <Cart cart={cart} />
     </div>
   );
 }
