ffdd7d2235fa410fe0214588526f468927f2b238
edivanmd <edivanmd@gmail.com>
2022-08-23 16:03:27 +1000

    Cart buttons

--- src/App.js
+++ src/App.js
@@ -21,8 +21,23 @@ function App() {
   };
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart)
+    const { cart } = await commerce.cart.add(productId, quantity);
+    setCart(cart);
+  }
+
+  const handleUpdateCartQuantity = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+    setCart(cart);
+  }
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
   }
   
   useEffect(() => {
@@ -38,7 +53,14 @@ function App() {
         <Navbar totalItems={cart.total_items} />
         <Routes>
           <Route path="/" element={<Products products={products} onAddToCart={handleAddToCart} />} />
-          <Route path="/cart" element={<Cart cart={cart} />} />
+          <Route path="/cart" element={
+            <Cart 
+              cart={cart} 
+              onUpdateCartQuantity={handleUpdateCartQuantity} 
+              onEmptyCart={handleEmptyCart}
+              onRemoveFromCart={handleRemoveFromCart}
+            />}
+          />
         </Routes>
       </div>
     </Router>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import CartItem from './CartItem/CartItem';
 
 import useStyles from './styles';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, onUpdateCartQuantity, onEmptyCart, onRemoveFromCart }) => {
     const classes = useStyles();
  
     const EmptyCart = () => (
@@ -18,14 +18,14 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} />
+                        <CartItem item={item} onUpdateCartQuantity={onUpdateCartQuantity} onRemoveFromCart={onRemoveFromCart} />
                     </Grid>
                 ))}
             </Grid>
             <div className={classes.cardDetails}>
                 <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={onEmptyCart}>Empty Cart</Button>
                     <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
                 </div>
             </div>
